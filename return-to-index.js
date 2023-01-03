ee01c26bfafd80b2f3f21d16ae258e1cd7047e75
krv1974 <63528758+krv1974@users.noreply.github.com>
2023-01-03 18:21:36 +1300

    Added index recall

---
+++ return-to-index.js
@@ -0,0 +1,13 @@
+const body = document.body;
+
+body.addEventListener('keypress', function(event) {
+    if (event.key === "i") {
+        let x = window.location.href.split("/")[3];
+        if (x == 'changelog') {
+            window.location.replace('index.html');
+        }
+        else {
+            window.location.replace('../index.html');
+        }
+    }
+})
