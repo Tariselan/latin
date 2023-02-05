9411bd5aa559e5ec29630f8200b3c2b3fca1f26e
krv1974 <63528758+krv1974@users.noreply.github.com>
2023-02-05 13:01:33 +1300

    made change

--- return-to-index.js
+++ return-to-index.js
@@ -1,7 +1,7 @@
 const body = document.body;
 
 body.addEventListener('keypress', function(event) {
-    if (event.key === "i") {
+    if (event.key === "0") {
         let x = window.location.href.split("/")[3];
         if (x == 'changelog') {
             window.location.replace('index.html');
