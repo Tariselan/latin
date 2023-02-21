342ae8038f34f183599a0bbbc065aaf3f3293d8c
Tariselan <woodmoreaaron@gmail.com>
2023-02-22 11:14:19 +1300

    Fix added

--- return-to-index.js
+++ return-to-index.js
@@ -2,12 +2,6 @@ const body = document.body;
 
 body.addEventListener('keypress', function(event) {
     if (event.key === "0") {
-        let x = window.location.href.split("/")[3];
-        if (x == 'changelog') {
-            window.location.replace('index.html');
-        }
-        else {
-            window.location.replace('../index.html');
-        }
+        window.location.replace('../index.html');
     }
 })
