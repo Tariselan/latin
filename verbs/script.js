e423f640724fd0c976083134033e60ab9c34b9f8
krv1974 <63528758+krv1974@users.noreply.github.com>
2023-02-26 14:22:06 +1300

    Added porto

--- verbs/script.js
+++ verbs/script.js
@@ -1,5 +1,19 @@
+const body = document.body;
+let sites = {
+    "r": "regular/index.html",
+    "i": "irregular/index.html"
+}
+
+let reg = {
+    "p": "porto/index.html",
+}
+
 //
 
 function goback() {
     window.location.replace('../index.html')
+}
+
+function goto(x) {
+    window.location.replace(x);
 }
