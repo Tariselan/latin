af0c79831533a9c10476edbcce9d6e4c3a0dd731
krv1974 <63528758+krv1974@users.noreply.github.com>
2023-02-27 17:49:23 +1300

    more javascript, still no functions

--- verbs/regular/porto/script.js
+++ verbs/regular/porto/script.js
@@ -1,3 +1,4 @@
+// consts and vars
 const userAnswer = document.getElementById('textbox');
 
 /*
@@ -13,205 +14,191 @@ const porto = {
     indicative: {
         active: {
             present: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
+                fs: 'porto',
+                ss: 'portas',
+                ts: 'portat',
+                fp: 'portamus',
+                sp: 'portatis',
+                tp: 'portant' 
             },
             imperfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port'
+                fs: 'portabam',
+                ss: 'portabas',
+                ts: 'portabat',
+                fp: 'portabamus',
+                sp: 'portabatis',
+                tp: 'portabant'
             },
             future: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port'
+                fs: 'portabo',
+                ss: 'portabis',
+                ts: 'portabit',
+                fp: 'portabimus',
+                sp: 'portabitis',
+                tp: 'portabunt'
             },
             perfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
+                fs: 'portavi',
+                ss: 'portavisti',
+                ts: 'portavit',
+                fp: 'portavimus',
+                sp: 'portavistis',
+                tp: 'portaverunt' 
             },
             pluperfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
+                fs: 'portaveram',
+                ss: 'portaveras',
+                ts: 'portaverat',
+                fp: 'portaveramus',
+                sp: 'portaveratis',
+                tp: 'portaverant' 
             },
             futurePerfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
+                fs: 'portavero',
+                ss: 'portaveris',
+                ts: 'portaverit',
+                fp: 'portaverimus',
+                sp: 'portaveritis',
+                tp: 'portaverint' 
             }
         },
         passive: {
             present: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
+                fs: 'portor',
+                ss: 'portaris',
+                ts: 'portatur',
+                fp: 'portamur',
+                sp: 'portamini',
+                tp: 'portantur' 
             },
             imperfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port'
+                fs: 'portabar',
+                ss: 'portabaris',
+                ts: 'portabatur',
+                fp: 'portabamur',
+                sp: 'portabamini',
+                tp: 'portabantur'
             },
             future: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port'
+                fs: 'portabor',
+                ss: 'portaberis',
+                ts: 'portabitur',
+                fp: 'portabimur',
+                sp: 'portabimini',
+                tp: 'portabuntur'
             },
             perfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
+                fs: 'portatus sum',
+                ss: 'portatus es',
+                ts: 'portatus est',
+                fp: 'portatus sumus',
+                sp: 'portatus estis',
+                tp: 'portatus sunt' 
             },
             pluperfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
+                fs: 'portatus eram',
+                ss: 'portatus eras',
+                ts: 'portatus erat',
+                fp: 'portatus eramus',
+                sp: 'portatus eratis',
+                tp: 'portatus erunt' 
             },
             futurePerfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
+                fs: 'portatus ero',
+                ss: 'portatus eris',
+                ts: 'portatus erit',
+                fp: 'portatus erimus',
+                sp: 'portatus eritis',
+                tp: 'portatus erunt' 
             }
         }
     },
     subjunctive: {
         active: {
             present: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
+                fs: 'portem',
+                ss: 'portes',
+                ts: 'portet',
+                fp: 'portemus',
+                sp: 'portetis',
+                tp: 'portent' 
             },
             imperfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port'
-            },
-            future: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port'
+                fs: 'portarem',
+                ss: 'portares',
+                ts: 'portaret',
+                fp: 'portaremus',
+                sp: 'portaretis',
+                tp: 'portent'
             },
             perfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
+                fs: 'portaverim',
+                ss: 'portaveris',
+                ts: 'portaverit',
+                fp: 'portaverimus',
+                sp: 'portaveritis',
+                tp: 'portaverint'
             },
             pluperfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
-            },
-            futurePerfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
+                fs: 'portavissem',
+                ss: 'portavisses',
+                ts: 'portavisset',
+                fp: 'portavissemus',
+                sp: 'portavissetis',
+                tp: 'portavissent' 
             }
         },
         passive: {
             present: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
+                fs: 'porter',
+                ss: 'porteris',
+                ts: 'portetur',
+                fp: 'portemur',
+                sp: 'portemini',
+                tp: 'portentur' 
             },
             imperfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port'
-            },
-            future: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port'
+                fs: 'portarer',
+                ss: 'portareris',
+                ts: 'portaretur',
+                fp: 'portaremur',
+                sp: 'portaremini',
+                tp: 'portarentur'
             },
             perfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
+                fs: 'portatus sim',
+                ss: 'portatus sis',
+                ts: 'portatus sit',
+                fp: 'portatus simus',
+                sp: 'portatus sitis',
+                tp: 'portatus sint' 
             },
             pluperfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
-            },
-            futurePerfect: {
-                fs: 'port',
-                ss: 'port',
-                ts: 'port',
-                fp: 'port',
-                sp: 'port',
-                tp: 'port' 
+                fs: 'portatus essem',
+                ss: 'portatus esses',
+                ts: 'portatus esset',
+                fp: 'portatus essemus',
+                sp: 'portatus essetis',
+                tp: 'portatus essent' 
             }
         }
-    },
+    }
 }
+
+const mood = ['indicative', 'subjunctive'];
+const mood_full = ['Indicative', 'Subjunctive'];
+
+const mood = [];
+const mood_full = [];
+
+const mood = [];
+const mood_full = [];
+
+const mood = [];
+const mood_full = [];
+
+// event listeners
+
+
+
+// functions
