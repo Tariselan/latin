41aaff643f9accde93bda0aa46f7a8fafc98d81f
krv1974 <63528758+krv1974@users.noreply.github.com>
2023-02-27 16:22:02 +1300

    sh*t tonne of code

---
+++ verbs/regular/porto/script.js
@@ -0,0 +1,115 @@
+const userAnswer = document.getElementById('textbox');
+
+/*
+fs = first singular
+ss = second singular
+ts = third singular
+fp = first plural
+sp = second plural
+tp = third plural
+*/
+
+const porto = {
+    indicative: {
+        active: {
+            present: {
+                fs: 'port',
+                ss: 'port',
+                ts: 'port',
+                fp: 'port',
+                sp: 'port',
+                tp: 'port' 
+            },
+            imperfect: {
+                fs: 'port',
+                ss: 'port',
+                ts: 'port',
+                fp: 'port',
+                sp: 'port',
+                tp: 'port'
+            },
+            future: {
+                fs: 'port',
+                ss: 'port',
+                ts: 'port',
+                fp: 'port',
+                sp: 'port',
+                tp: 'port'
+            },
+            perfect: {
+                fs: 'port',
+                ss: 'port',
+                ts: 'port',
+                fp: 'port',
+                sp: 'port',
+                tp: 'port' 
+            },
+            pluperfect: {
+                fs: 'port',
+                ss: 'port',
+                ts: 'port',
+                fp: 'port',
+                sp: 'port',
+                tp: 'port' 
+            },
+            futurePerfect: {
+                fs: 'port',
+                ss: 'port',
+                ts: 'port',
+                fp: 'port',
+                sp: 'port',
+                tp: 'port' 
+            }
+        },
+        subjunctive: {
+            present: {
+                fs: 'port',
+                ss: 'port',
+                ts: 'port',
+                fp: 'port',
+                sp: 'port',
+                tp: 'port' 
+            },
+            imperfect: {
+                fs: 'port',
+                ss: 'port',
+                ts: 'port',
+                fp: 'port',
+                sp: 'port',
+                tp: 'port'
+            },
+            future: {
+                fs: 'port',
+                ss: 'port',
+                ts: 'port',
+                fp: 'port',
+                sp: 'port',
+                tp: 'port'
+            },
+            perfect: {
+                fs: 'port',
+                ss: 'port',
+                ts: 'port',
+                fp: 'port',
+                sp: 'port',
+                tp: 'port' 
+            },
+            pluperfect: {
+                fs: 'port',
+                ss: 'port',
+                ts: 'port',
+                fp: 'port',
+                sp: 'port',
+                tp: 'port' 
+            },
+            futurePerfect: {
+                fs: 'port',
+                ss: 'port',
+                ts: 'port',
+                fp: 'port',
+                sp: 'port',
+                tp: 'port' 
+            }
+        }
+    }
+}
